ddcc1b259b62b8a00c228b4a21ab4b923a55e38c
DESKTOP-LVG3KK4\DELL <chung.nguyenchung@hcmut.edu.vn>
2023-12-20 00:07:25 +0700

    Update

---
+++ STM/Lab4/Core/Inc/addTask.h
@@ -0,0 +1,21 @@
+/*
+ * addTask.h
+ *
+ *  Created on: Dec 19, 2023
+ *      Author: DELL
+ */
+
+#ifndef INC_ADDTASK_H_
+#define INC_ADDTASK_H_
+
+#include "main.h"
+
+void led_Red_test();
+void led_Yellow_test();
+void led_Green_test();
+void led_Purple_test();
+void led_White_test();
+void led_Blue_test();
+void button_1();
+
+#endif /* INC_ADDTASK_H_ */
